18d857036263441b13971289bdf70c0d1e616d12
Michael Sainz <michaelsainz@github.com>
2020-08-10 11:49:04 -0700

    Remove CodeSpaces default .bashrc file and link.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 # Bash symlinking for Codespaces
+# -----------------------------------------------------------------------------
 if [ "$CODESPACES" == "true" ]
 then
   echo "CodeSpaces environment detected"
+  rm ~/.bashrc
   ln -s ~/dotfiles/bash/.bashrc ~/.bashrc
 else
 # Bash symlinking for generic environments
