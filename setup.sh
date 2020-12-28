b95ba4612727941444d33a0619be1e2cacc12f5a
Michael Sainz <michaelsainz@github.com>
2020-12-27 21:32:04 -0800

    Moving some blocks around

--- setup.sh
+++ setup.sh
@@ -8,23 +8,6 @@ then
   CodeSpacesDotfilesConfig
 fi
 
-# Parameters
-while true; do
-  case "$1" in
-    -i|--init)
-      echo "Install/initialize software"
-      DotfilesConfig
-      InstallSoftware
-      shift
-    ;;
-    -d|--dotfiles)
-     DotfilesConfig
-  *)
-    break
-    ;;
-  esac
-done
-
 InstallSoftware {
   if type xcode-select >&- && xpath=$( xcode-select --print-path ) && test -d "${xpath}" && test -x "${xpath}" ; then
     echo "Xcode CLI Tools are installed"
@@ -47,3 +30,22 @@ DotfilesConfig {
   echo "Generic environment detected"
   ln -s ~/Repos/dotfiles/bash/.bashrc ~/.bashrc
 }
+
+# Parameters
+while true; do
+  case "$1" in
+    -i|--init)
+      echo "Install/initialize software"
+      DotfilesConfig
+      InstallSoftware
+      shift
+    ;;
+    -d|--dotfiles)
+     DotfilesConfig
+     shift
+    ;;
+    *)
+    break
+    ;;
+  esac
+done
